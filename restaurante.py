952712dd5252fda0b4a0cb4372d8cde94a73e03f
VictorPxx <victordev.luz@gmail.com>
2024-08-19 13:14:19 -0300

    initial commit

---
+++ restaurante.py
@@ -0,0 +1,16 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+    
+restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Soares Lanches'
+restaurante_praca.categoria = 'Lanchonete'
+
+restaurante_pizza = Restaurante()
+restaurante_pizza.nome = 'Tarantella'
+restaurante_pizza.categoria = 'Pizzaria'
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(vars(restaurante_pizza))
