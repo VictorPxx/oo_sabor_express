6f85c8e30dce71fd2f7c8dfdfabf56cf5946a805
VictorPxx <victordev.luz@gmail.com>
2024-08-22 20:24:15 -0300

    add função para listar restaurantes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,14 +1,21 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
     
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Soares Lanches'
-restaurante_praca.categoria = 'Lanchonete'
+    restaurantes = []
+    
+    def __init__(self, nome='', categoria=''):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+        
+    def __str__(self):
+        return f'{self.nome} {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo}')
+    
+restaurante_1 = Restaurante(nome='Soares Lanches', categoria='Hamburgueria')
+restaurante_2 = Restaurante(nome='WR', categoria='Pizzaria')
 
-restaurante_pizza = Restaurante()
-restaurante_pizza.nome = 'Tarantella'
-restaurante_pizza.categoria = 'Pizzaria'
-
-restaurantes = [restaurante_praca, restaurante_pizza]
+Restaurante.listar_restaurantes()
